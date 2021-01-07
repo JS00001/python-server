e68048fa6ded5895708e883db6fe724ff18481ec
JackS0001 <jse15471@oconeeschools.org>
2021-01-07 16:27:26 -0500

    Discord OAuth

--- main.py
+++ main.py
@@ -3,24 +3,40 @@ Python Authentication API
 Author: JS
 """
 
-from os import getenv
+from os import getenv, environ
 from flask import Flask, jsonify
 from flask_pymongo import PyMongo
+from flask_discord import DiscordOAuth2Session
 from dotenv import load_dotenv
-from routes.user import user_blueprint
-from routes.login import login_blueprint
 
 load_dotenv()
 app = Flask(__name__)
+app.secret_key = getenv("SESSION_SECRET")
+
+#   Flask Configuration
+environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
 app.config["MONGO_URI"] = getenv("MONGO_URL")
-app.register_blueprint(login_blueprint)
+app.config["DISCORD_CLIENT_ID"] = getenv("CLIENT_ID")
+app.config["DISCORD_REDIRECT_URI"] = getenv("REDIRECT_URL")
+app.config["DISCORD_CLIENT_SECRET"] = getenv("CLIENT_SECRET")
+
+#   Exported Variables
 mongo = PyMongo(app).db
+discord = DiscordOAuth2Session(app)
 
+#   Flask Routes
+from routes.user import user_blueprint
+from routes.login import login_blueprint
 
+app.register_blueprint(login_blueprint)
+app.register_blueprint(user_blueprint)
+
+#   Error Handling (404)
 @app.errorhandler(404)
 def page_not_found(e):
     return jsonify({"error": True, "message": 404})
 
 
+#   Start Server
 if __name__ == "__main__":
     app.run(debug=True)
--- routes/login.py
+++ routes/login.py
@@ -1,13 +1,25 @@
-from os import getenv
-from flask import Blueprint, jsonify, session
-from requests_oauthlib import OAuth2Session
+from main import discord
+from flask import Blueprint, jsonify, redirect
+from flask_discord import requires_authorization
 
-login_blueprint = Blueprint('login_blueprint', __name__)
-scope = ['identify', 'email']
+login_blueprint = Blueprint("login_blueprint", __name__)
 
-
-@login_blueprint.route("/login", methods=["GET"])
+# Login Route (Redirects to Discord)
+@login_blueprint.route("/login")
 def login():
-    oauth = OAuth2Session(getenv("CLIENT_ID"), redirect_uri=getenv("REDIRECT_URL"), scope=scope)
-    login_url, state = oauth.authorization_url("https://discordapp.com/api/oauth2/authorize")
-    return jsonify({"test": True})
+    return discord.create_session()
+
+
+# Callback Route (Processes Data)
+@login_blueprint.route("/login/callback")
+def callback():
+    discord.callback()
+    return redirect("http://localhost:5000/login/profile")
+
+
+# Profile Route (View user data)
+@login_blueprint.route("/login/profile")
+@requires_authorization
+def profile():
+    user = discord.fetch_user()
+    return user.name
