da292866e71abdb43ebc1a52417507c96d24b32d
JackS0001 <jse15471@oconeeschools.org>
2021-01-08 15:27:31 -0500

    User endpoint and profile information

--- routes/logout.py
+++ routes/logout.py
@@ -4,6 +4,7 @@ from flask_discord import requires_authorization
 
 logout_blueprint = Blueprint("logout_blueprint", __name__)
 
+
 @logout_blueprint.route("/logout")
 @requires_authorization
 def logout():
