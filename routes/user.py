1f24bfd939820370e17d79ecf207ee6c662addcb
JackS0001 <jse15471@oconeeschools.org>
2021-01-08 15:24:37 -0500

    User endpoint and profile information

--- routes/user.py
+++ routes/user.py
@@ -1,11 +1,11 @@
 from main import dbusers
 from flask import Blueprint, jsonify
 
-user_blueprint = Blueprint('user_blueprint', __name__)
+user_blueprint = Blueprint("user_blueprint", __name__)
 
 
-@user_blueprint.route('/user/<user_id>')
+@user_blueprint.route("/user/<user_id>")
 def user(user_id):
-   data = dbusers.find_one_or_404({"id": int(user_id)})
-   del data["_id"]
-   return data
+    data = dbusers.find_one_or_404({"id": int(user_id)})
+    del data["_id"]
+    return data
